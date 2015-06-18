63495e5a7fbe5e2d7222511af242d48eb4f80222
Jim Patt & Drew Finstrom <student@epicodus.com>
2015-06-18 12:26:38 -0700

    adds transtion animations

--- app/transitions.js
+++ app/transitions.js
@@ -4,4 +4,21 @@ export default function(){
     this.use('toLeft'),
     this.reverse('toRight')
   );
+
+  this.transition(
+    this.childOf('.liquid-ul'),
+    this.use('toDown')
+  );
+
+  this.transition(
+    this.hasClass('liquid-if'),
+    this.toValue(true),
+    this.use('toLeft')
+  );
+
+  this.transition(
+    this.hasClass('liquid-if'),
+    this.toValue(false),
+    this.use('toRight')
+  );
 }
